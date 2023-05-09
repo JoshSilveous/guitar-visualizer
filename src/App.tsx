09fa916962829b1c134d0340f1ec4fa00027e8fe
Joshua Silveous <JoshSilveous@gmail.com>
2023-05-09 13:08:51 -0400

    installed sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 import { ScaleSelector } from './scale/ScaleSelector'
 import { genScaleInfo } from './scale/genScaleInfo'
 
