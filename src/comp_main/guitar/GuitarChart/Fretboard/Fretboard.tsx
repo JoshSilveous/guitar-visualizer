60da54810e5bd8ed503a08aac9d8c6e10862d26c
Joshua Silveous <JoshSilveous@gmail.com>
2023-05-19 16:33:33 -0400

    fleshed out fretboard component

--- src/comp_main/guitar/GuitarChart/Fretboard/Fretboard.tsx
+++ src/comp_main/guitar/GuitarChart/Fretboard/Fretboard.tsx
@@ -1,3 +1,6 @@
+import { getLet } from '../../../scale/scale'
+import { generateString } from '../../guitar'
+
 interface FretboardProps {
     scaleInfo: ScaleInfo
     highlightState: { notes: boolean[]; chords: boolean[] }
@@ -16,14 +19,50 @@ export function Fretboard({ scaleInfo, highlightState, highlightCtrl, guitarSett
 
     console.log(guitarSettings.stringOpens)
 
-    let test = guitarSettings.stringOpens.map((ex) => {
-        return <div style={{ width: '30px' }}>{ex}</div>
+    const strings = guitarSettings.stringOpens.map((openNote) => {
+        return generateString(openNote, guitarSettings.stringLength + 1)
+    })
+
+    const stringDisplay = strings.reverse().map((string) => {
+        const noteDisplay = string.map((note) => {
+            const isHighlighted = highlightState.notes[scaleInfo.scale.num.indexOf(note)]
+            const isInScale = scaleInfo.scale.num.includes(note)
+
+            let className = 'note'
+            if (isHighlighted) {
+                className += ' highlighted'
+            }
+            if (!isInScale) {
+                className += ' not-in-scale'
+            }
+
+            function toggleHighlight() {
+                if (isHighlighted) {
+                    highlightCtrl.unhighlightNote(scaleInfo.scale.num.indexOf(note))
+                } else {
+                    highlightCtrl.highlightNote(scaleInfo.scale.num.indexOf(note))
+                }
+            }
+
+            return (
+                <div className="note-container">
+                    <div className={className} onClick={toggleHighlight}>
+                        <div className="note-text">{getLet(note, scaleInfo.isSharp)}</div>
+                    </div>
+                </div>
+            )
+        })
+        return (
+            <>
+                <div className="string">{noteDisplay}</div>
+                <div className="string-display"></div>
+            </>
+        )
     })
 
     return (
         <div className="fretboard-container">
-            {guitarSettings.stringLength} <br />
-            {test}
+            <div className="fretboard">{stringDisplay}</div>
         </div>
     )
 }
