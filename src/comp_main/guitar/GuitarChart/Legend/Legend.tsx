89debcc6efa7049de4b54b5d7173592482da2d41
Joshua Silveous <JoshSilveous@gmail.com>
2023-05-25 16:31:52 -0400

    edited legend style

--- src/comp_main/guitar/GuitarChart/Legend/Legend.tsx
+++ src/comp_main/guitar/GuitarChart/Legend/Legend.tsx
@@ -1,33 +1,41 @@
 interface LegendProps {
     scaleInfo: ScaleInfo
+    highlightState: HighlightState
 }
-export function Legend({ scaleInfo }: LegendProps) {
+export function Legend({ scaleInfo, highlightState }: LegendProps) {
     if (false) {
-        console.log(scaleInfo)
+        console.log(scaleInfo, highlightState)
     }
 
     const tonicNote = scaleInfo.tonic.let
     const significantNote = scaleInfo.mode.significantNotes
         ? scaleInfo.scale.let[scaleInfo.mode.significantNotes[0]]
         : null
+    const isHighlightingChord = !highlightState.chords.every((val) => val === false)
 
     console.log(scaleInfo.mode.significantNotes)
 
     return (
-        <div className="legend-container">
+        <div className="legend">
             <h2>Legend</h2>
-            <div className="row">
-                <div className="note tonic">{tonicNote}</div>
-                <div className="label">Tonic note of scale</div>
-            </div>
-            {significantNote && (
+            <div className="legend-container">
                 <div className="row">
-                    <div className="note significant">{significantNote}</div>
-                    <div className="label">
-                        Significant note, emphasize while soloing to bring out the mode's unique sounds.
+                    <div className={`note tonic`}>
+                        <div className="note-text">{tonicNote}</div>
                     </div>
+                    <div className="label">Tonic note of scale</div>
                 </div>
-            )}
+                {significantNote && (
+                    <div className="row">
+                        <div className="note significant">
+                            <div className="note-text">{significantNote}</div>
+                        </div>
+                        <div className="label">
+                            Significant note, emphasize while soloing to bring out the mode's unique sounds.
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
